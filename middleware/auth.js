0e07f1978da0089586d90c6f10054fc9e90a10b6
VictorB <victorb62@byui.edu>
2023-06-13 17:30:43 -0600

    week8

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,7 @@ const isLoggedIn = (req, res, next) => {
     if (req.user) {
       next();
     } else {
-      res.status(401).send('<body style="background: linear-gradient(0deg, rgb(84, 80, 211) 0%, rgb(2, 35, 58) 100%);"><div style="text-align:center;font-size:24pt;font-family:sans-serif; background-color:white; width: 40%; margin:auto; margin-top:150px; padding:40px; border-radius:15px;line-height:2;" > Not Logged In <br> Access Denied <br> <a href="../" style="background-color:#aaf;color:white;padding:10px;border-radius:15px;margin:10px;text-decoration:none;"> Click to return to Log in Page </a> </div>');
-     
+       res.status(401).send('<body style="background:black;"><div style="text-align:center;font-size:20px;font-family:sans-serif;background-color:white;width:350px;margin:auto;margin-top:200px;padding:40px;border-radius:30px;line-height:2;"> Not Logged In <br> Access Denied <br><br><a href="../" style="background:rgb(84, 80, 211);color:white;padding:10px;border-radius:20px;margin:10px;text-decoration:none;"> Click to return to Log in Page </a> </div>');
     }
   }
   
